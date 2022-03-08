c3d6a1c66d797e4ee5a2596242e4f723b43bd0bf
Felipe Alexander Borges <87782781+prazerfelipe@users.noreply.github.com>
2022-03-08 20:12:16 -0300

    Add files via upload

---
+++ Desafio/Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,39 @@
+using static Series.Interfaces.IRepositorio;
+using System.Collections.Generic;
+
+namespace Series
+{
+    public class SerieRepositorio : IRepositorio<Series>
+    {
+        private List<Series> listaSerie = new List<Series>();
+		public void Atualiza(int id, Series objeto)
+		{
+			listaSerie[id] = objeto;
+		}
+
+		public void Exclui(int id)
+		{
+			listaSerie[id].Excluir();
+		}
+
+		public void Insere(Series objeto)
+		{
+			listaSerie.Add(objeto);
+		}
+
+		public List<Series> Lista()
+		{
+			return listaSerie;
+		}
+
+		public int ProximoId()
+		{
+			return listaSerie.Count;
+		}
+
+		public Series RetornaPorId(int id)
+		{
+			return listaSerie[id];
+		}
+    }
+}
